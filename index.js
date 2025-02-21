82d647bfe992b770433c81c9db69130616f0ea28
crodpar637 <crodpar637@ieshnosmachado.org>
2025-02-21 12:12:23 +0100

    Auto save changes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Importar libreria para manejo de ficheros de configuración
+// Importar libreria para manejo de ficheros de configuración dependiendo de la variable de entorno NODE_ENV
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV || "development"}`,
 });
+
 // Importar fichero de configuración con variables de entorno
 const config = require("./config/config");
 // Importar librería express --> web server
@@ -23,14 +24,14 @@ const app = express();
 app.use(express.json());
 
 // Configurar CORS para admitir cualquier origen
-// app.use(cors()); // No permitiría el envío de cookies porque una API pública
+// app.use(cors()); // No permitite el envío de cookies en una API pública
 
 if (process.env.NODE_ENV !== "production") {
   // Configurar CORS para admitir el origen del frontend en desarrollo
   app.use(
     cors({
-      // origin: "http://localhost:5173", // Permitir el frontend en desarrollo de React
-      origin: "http://localhost:8081", // Permitir el frontend en desarrollo de React Native
+      origin: "http://localhost:5173", // Permitir el frontend en desarrollo de React
+      //origin: "http://localhost:8081", // Permitir el frontend en desarrollo de React Native
       credentials: true, // Permitir envío de cookies
     })
   );
@@ -45,22 +46,29 @@ app.use("/api/pedidos", pedidoRoutes);
 app.use("/api/users", userRoutes);
 
 //Ruta para manejar las solicitudes al archivo index.html
+
 // app.get('/', (req, res) => {
 if (process.env.NODE_ENV !== "production") {
+  console.log("Sirviendo ficheros de desarrollo");
   // Configurar el middleware para servir archivos estáticos desde el directorio public/dev en desarrollo
   app.use(express.static(path.join(__dirname, "public/dev")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/dev", "index.html"));
   });
 } else {
+  console.log("Sirviendo ficheros de producción");
   // Configurar el middleware para servir archivos estáticos desde el directorio public/dev en producción
-  app.use(express.static(path.join(__dirname, "public/dev")));
+  app.use(express.static(path.join(__dirname, "public/prod")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/prod", "index.html"));
   });
 }
 
+
 // Iniciar el servidor solo si no estamos en modo de prueba
+// en modo de prueba, el servidor se inicia en el archivo de prueba
 if (process.env.NODE_ENV !== "test") {
   app.listen(config.port, () => {
     console.log(`Servidor escuchando en el puerto ${config.port}`);
